ea5020a32bad443525c841ba5c770c934a106ee5
RH <RENE.H.OK@GMAIL.COM>
2019-01-04 15:46:08 +1100

    test

--- public/js/index.js
+++ public/js/index.js
@@ -8,5 +8,16 @@ socket.on('disconnect', () =>{
 })
 
 socket.on('newMessage', (message) =>{
-    console.log('New message', message);
+    // console.log('New message', message);
+    var li = jQuery('<li><li>');
+    li.text(`${message.from}:${message.text}`)
+    jQuery('#messages').append(li);
+})
+
+jQuery('#message-form').on('submit', (e)=>{
+    e.preventDefault();
+    socket.emit('createMessage',{
+        from:'User',
+        text:jQuery('[name=message]').val()
+    },()=>{});
 })
--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const {generateMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
@@ -15,20 +15,21 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
     // Admin welcome message
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat room'))
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat room'));
     //Admin text New user joined
-    socket.broadcast.emit('newMessage', generateMessage('Admin','New user joined!'))
+    socket.broadcast.emit('newMessage', generateMessage('Admin','New user joined!'));
 
     //listen on the createmessage emit
-    socket.on('createMessage', (message) => {
-        console.log('createMessage', message)
+    socket.on('createMessage', (message, callback) => {
+        console.log('createMessage', message);
         //send message to everyone
-        io.emit('newMessage',generateMessage(message.from,message.text)) 
-    })
+        io.emit('newMessage',generateMessage(message.from,message.text)); 
+        callback('this is from server');
+    });
 
     socket.on('disconnect', () => {
         console.log('Disconnected from server')
-    })
+    });
 })
 
 server.listen(port, () => {
