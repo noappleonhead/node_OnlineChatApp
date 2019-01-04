4f82f7d75d1f78d7826ee22c757503cc42a4f188
RH <RENE.H.OK@GMAIL.COM>
2019-01-04 14:48:42 +1100

    generate message utis

--- public/js/index.js
+++ public/js/index.js
@@ -7,7 +7,6 @@ socket.on('disconnect', () =>{
   console.log('Disconnected from server');
 })
 
-
 socket.on('newMessage', (message) =>{
     console.log('New message', message);
 })
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const socketIO = require('socket.io');
 const express = require('express');
+const {generateMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000
@@ -13,15 +14,16 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
+    // Admin welcome message
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat room'))
+    //Admin text New user joined
+    socket.broadcast.emit('newMessage', generateMessage('Admin','New user joined!'))
 
     //listen on the createmessage emit
-    socket.on('createMessage',(message)=>{
-        console.log('createMessage',message)
-        io.emit('newMessage',{
-            from:message.from,
-            text:message.text,
-            createdAt:new Date().getTime()
-        })//send message to everyone
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message)
+        //send message to everyone
+        io.emit('newMessage',generateMessage(message.from,message.text)) 
     })
 
     socket.on('disconnect', () => {
